23bd868f5c2bdbbe9577c7e95243dc60cd70cb4f
davidmoten <davidmoten@7688b277-2b4d-0410-bddd-c1844407bb12>
2008-06-05 10:17:16 +0000

    filling out Shop system

--- xUmlCompiler/src/java/moten/david/uml/xuml/model/example/shop/ShopInjectorModule.java
+++ xUmlCompiler/src/java/moten/david/uml/xuml/model/example/shop/ShopInjectorModule.java
@@ -1,12 +1,18 @@
 package moten.david.uml.xuml.model.example.shop;
 
+import shop.actions.CustomerActions;
+import shop.actions.OrderActions;
+import shop.actions.ShipmentActions;
+
 import com.google.inject.AbstractModule;
 
 public class ShopInjectorModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		// bind(RequestActions.class).to(RequestBehaviour.class);
+		bind(OrderActions.class).to(OrderBehaviour.class);
+		bind(ShipmentActions.class).to(ShipmentBehaviour.class);
+		bind(CustomerActions.class).to(CustomerBehaviour.class);
 	}
 
 }
