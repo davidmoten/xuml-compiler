1c962ab037462071cc511a2e9af87faf452bb60d
davidmoten <davidmoten@7688b277-2b4d-0410-bddd-c1844407bb12>
2008-06-10 13:18:53 +0000

    refactored uml/xuml to xuml

--- trunk/xUmlCompiler/src/java/moten/david/uml/xuml/model/CodeGenerator.java
+++
@@ -1,5 +0,0 @@
-package moten.david.uml.xuml.model;
-
-public interface CodeGenerator {
-	public void generate(String outputDirectory) throws Exception;
-}
--- trunk/xUmlCompiler/src/java/moten/david/uml/xuml/model/compiler/templates/ResourceLocator.java
+++
@@ -1,5 +0,0 @@
-package moten.david.uml.xuml.model.compiler.templates;
-
-public class ResourceLocator {
-
-}
