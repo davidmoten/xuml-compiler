6e0bf486febd1d8f972797bb70cb5bb09dcc38fb
davidmoten <davidmoten@7688b277-2b4d-0410-bddd-c1844407bb12>
2008-06-18 09:08:25 +0000

    converted to ecore generated data model for SystemViewer

---
+++ xUmlCompiler/src/viewer/model/generated/view/impl/ViewportImpl.java
@@ -0,0 +1,43 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: ViewportImpl.java,v 1.1 2008/06/18 06:18:13 dxm Exp $
+ */
+package view.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import view.ViewPackage;
+import view.Viewport;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Viewport</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class ViewportImpl extends DimensionImpl implements Viewport {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ViewportImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return ViewPackage.Literals.VIEWPORT;
+	}
+
+} //ViewportImpl
