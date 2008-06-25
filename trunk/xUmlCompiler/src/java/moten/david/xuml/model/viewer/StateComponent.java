90acd422be33d6ebb018f1616bbdbc3f155b402c
davidmoten <davidmoten@7688b277-2b4d-0410-bddd-c1844407bb12>
2008-06-25 09:51:17 +0000

    Persisted positions on State Diagram

--- trunk/xUmlCompiler/src/java/moten/david/xuml/model/viewer/StateComponent.java
+++ trunk/xUmlCompiler/src/java/moten/david/xuml/model/viewer/StateComponent.java
@@ -6,10 +6,17 @@ import model.Stately;
 
 public class StateComponent extends Container {
 
+	private Stately stately;
+
 	public StateComponent(Stately stately) {
 		super(Color.white, true, new String[] { stately.getName()
 				+ Container.BOLD + Container.CENTRE });
+		this.stately = stately;
 		Movable.makeMovable(this);
+	}
+
+	public Stately getStately() {
+		return stately;
 	}
 
 }
