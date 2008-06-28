08f92e041bd3de8b5e7ab5ab28e15b73aa563800
davidmoten <davidmoten@7688b277-2b4d-0410-bddd-c1844407bb12>
2008-06-28 13:56:33 +0000

    fix background color for initial state display in linux

--- xUmlCompiler/src/java/moten/david/xuml/model/viewer/StateTerminatorComponent.java
+++ xUmlCompiler/src/java/moten/david/xuml/model/viewer/StateTerminatorComponent.java
@@ -13,13 +13,13 @@ public class StateTerminatorComponent extends StateComponent {
 	private static final long serialVersionUID = -371185281464336368L;
 	private static int baseSize = 14;
 	private static int gap = 3;
-	private static int ringWidth = 1;
 	private boolean initial;
 
 	public StateTerminatorComponent(Stately state, boolean initial) {
 		super(state);
+		this.removeAll();
 		this.initial = initial;
-		int total = baseSize + gap + ringWidth;
+		int total = baseSize + gap + 1;
 		setSize(total, total);
 		setBorder(null);
 		setBackground(new JPanel().getBackground());
@@ -27,10 +27,11 @@ public class StateTerminatorComponent extends StateComponent {
 
 	@Override
 	public void paint(Graphics g) {
-		super.paint(g);
+		// super.paint(g);
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setBackground(getBackground());
 		g.clearRect(0, 0, getWidth(), getHeight());
 		g.fillOval(gap, gap, baseSize - 2, baseSize - 2);
 		if (!initial) {
