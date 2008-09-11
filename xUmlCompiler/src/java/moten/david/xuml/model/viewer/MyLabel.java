fc8f8dbc48aceeac5382eea1956c2b61f594a46c
davidmoten <davidmoten@7688b277-2b4d-0410-bddd-c1844407bb12>
2008-09-11 23:52:47 +0000

    enhanced MyLabel so it shrinks its border

--- xUmlCompiler/src/java/moten/david/xuml/model/viewer/MyLabel.java
+++ xUmlCompiler/src/java/moten/david/xuml/model/viewer/MyLabel.java
@@ -1,10 +1,14 @@
 package moten.david.xuml.model.viewer;
 
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.text.BreakIterator;
 
 import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
 
 public class MyLabel extends JLabel {
 
@@ -12,16 +16,18 @@ public class MyLabel extends JLabel {
 
 	private static final String FONT_NAME = "monospace";
 	private static final int FONT_SIZE = 10;
+	private static final int MAX_WIDTH = 100;
 
 	private final String key;
 
-	public MyLabel(String key, String label) {
-		super("<html><p>" + label + "</p></html>");
-		this.key = key;
-		Movable.makeMovable(this);
+	public MyLabel(String key, String text) {
+		super();
 		setSize(100, 40);
 		setFont(new Font(FONT_NAME, Font.PLAIN, FONT_SIZE));
-
+		wrapLabelText(this, text);
+		// setBorder(BorderFactory.createLineBorder(Color.lightGray));
+		this.key = key;
+		Movable.makeMovable(this);
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -32,6 +38,49 @@ public class MyLabel extends JLabel {
 				}
 			}
 		});
+	}
+
+	private void wrapLabelText(JLabel label, String text) {
+		FontMetrics fm = label.getFontMetrics(label.getFont());
+		BreakIterator boundary = BreakIterator.getWordInstance();
+		boundary.setText(text);
+
+		StringBuffer trial = new StringBuffer();
+		StringBuffer real = new StringBuffer("<html>");
+
+		int numLines = 1;
+		int start = boundary.first();
+		int maxWidth = 0;
+		int trialWidth = 0;
+		for (int end = boundary.next(); end != BreakIterator.DONE; start = end, end = boundary
+				.next()) {
+			String word = text.substring(start, end);
+			int beforeTrialWidth = SwingUtilities.computeStringWidth(fm, trial
+					.toString());
+			trial.append(word);
+			trialWidth = SwingUtilities
+					.computeStringWidth(fm, trial.toString());
+			if (trialWidth > MAX_WIDTH) {
+				if (beforeTrialWidth > maxWidth)
+					maxWidth = beforeTrialWidth;
+				trial = new StringBuffer(word);
+				real.append("<br>");
+				numLines++;
+			}
+			real.append(word);
+		}
+		int lastLineLength = SwingUtilities.computeStringWidth(fm, trial
+				.toString());
+		if (lastLineLength > maxWidth)
+			maxWidth = lastLineLength;
+		if (numLines == 1)
+			maxWidth = SwingUtilities.computeStringWidth(fm, text);
+		real.append("</html>");
+		label.setText(real.toString());
+		Dimension newSize = new Dimension();
+		newSize.height = numLines * fm.getHeight();
+		newSize.width = maxWidth + 5;
+		label.setSize(newSize);
 	}
 
 	public String getKey() {
