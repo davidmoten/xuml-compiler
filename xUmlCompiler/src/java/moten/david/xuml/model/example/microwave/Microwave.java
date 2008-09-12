14842ceaf52295a72922094499bdca8f365e514c
davidmoten <davidmoten@7688b277-2b4d-0410-bddd-c1844407bb12>
2008-09-12 07:17:48 +0000

    generate sample persistence.xml

--- xUmlCompiler/src/java/moten/david/xuml/model/example/microwave/Microwave.java
+++ xUmlCompiler/src/java/moten/david/xuml/model/example/microwave/Microwave.java
@@ -60,7 +60,7 @@ public class Microwave extends SystemBase {
 			outputDirectoryFilename = args[0];
 		Microwave system = new Microwave();
 		system.view("src/viewer/Microwave.ecore");
-		system.generate(outputDirectoryFilename);
+		// system.generate(outputDirectoryFilename);
 	}
 
 }
