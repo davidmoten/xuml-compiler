db0d212065d7f45dbc15138e30dc9ef4e60acb63
davidmoten <davidmoten@7688b277-2b4d-0410-bddd-c1844407bb12>
2010-02-15 02:14:34 +0000

    update to use xuml-compiler 4.6.8

--- trunk/examples/simple/src/test/java/moten/david/xuml/model/example/simple/SimpleGenerator.java
+++ trunk/examples/simple/src/test/java/moten/david/xuml/model/example/simple/SimpleGenerator.java
@@ -3,6 +3,7 @@ package moten.david.xuml.model.example.simple;
 public class SimpleGenerator {
 	public static void main(String[] args) throws Exception {
 		Simple simple = new Simple();
-		simple.generate("temp/simple");
+		String o = "target/simple";
+		simple.generate(o, o, o, o);
 	}
 }
