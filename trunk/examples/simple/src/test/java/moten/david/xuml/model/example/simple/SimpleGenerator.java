b1131ab27bc6d1b1a344f6d21397793654c49b0c
davidmoten <davidmoten@7688b277-2b4d-0410-bddd-c1844407bb12>
2010-02-16 21:39:23 +0000

    split simple into defintion and main artifacts

--- trunk/examples/simple/src/test/java/moten/david/xuml/model/example/simple/SimpleGenerator.java
+++
@@ -1,9 +0,0 @@
-package moten.david.xuml.model.example.simple;
-
-public class SimpleGenerator {
-	public static void main(String[] args) throws Exception {
-		Simple simple = new Simple();
-		String o = "target/simple";
-		simple.generate(o, o, o, o);
-	}
-}
