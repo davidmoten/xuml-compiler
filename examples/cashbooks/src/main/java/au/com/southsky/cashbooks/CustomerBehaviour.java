9bd52764e03c0d51e9af0385b5de407ba98ab2e6
davidmoten <davidmoten@7688b277-2b4d-0410-bddd-c1844407bb12>
2010-02-21 01:19:01 +0000

    set shortname to name in customer behaviour

--- examples/cashbooks/src/main/java/au/com/southsky/cashbooks/CustomerBehaviour.java
+++ examples/cashbooks/src/main/java/au/com/southsky/cashbooks/CustomerBehaviour.java
@@ -10,36 +10,36 @@ import cashbooks.actions.CustomerActions;
 
 public class CustomerBehaviour implements CustomerActions {
 
-        private Customer customer;
+	private Customer customer;
 
-        @Override
-        public void setCustomer(Customer customer) {
-                this.customer = customer;
-        }
+	@Override
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
 
-        @Override
-        public void performOnEntryActive(EventActivate event) {
-        }
+	@Override
+	public void performOnEntryActive(EventActivate event) {
+	}
 
-        @Override
-        public void performOnEntryInactive(EventDeactivate event) {
-        }
+	@Override
+	public void performOnEntryInactive(EventDeactivate event) {
+	}
 
-        @Override
-        public void performOnEntryActive(EventAddEmail event) {
-                // establish R1
-                event.getEmail().setCustomer(customer);
-        }
+	@Override
+	public void performOnEntryActive(EventAddEmail event) {
+		// establish R1
+		event.getEmail().setCustomer(customer);
+	}
 
-        @Override
-        public void performOnEntryFinalState(EventRemove event) {
-                // TODO remove an entity (will need to inject an entity manager)
-        }
+	@Override
+	public void performOnEntryFinalState(EventRemove event) {
+		// TODO remove an entity (will need to inject an entity manager)
+	}
 
-        @Override
-        public void performOnEntryInactive(EventNewCustomer event) {
-                customer.setName(event.getName());
-        }
+	@Override
+	public void performOnEntryInactive(EventNewCustomer event) {
+		customer.setName(event.getName());
+		customer.setShortname(event.getName());
+	}
 
 }
-
--- examples/cashbooks/src/test/java/au/com/southsky/cashbooks/Test.java
+++ examples/cashbooks/src/test/java/au/com/southsky/cashbooks/Test.java
@@ -21,80 +21,80 @@ import com.google.inject.Injector;
 
 public class Test {
 
-        private Logger log = Logger.getLogger(Test.class);
+	private final Logger log = Logger.getLogger(Test.class);
 
-        private static Injector injector = Guice
-                        .createInjector(new CashbooksInjectorModule());
+	private static Injector injector = Guice
+			.createInjector(new CashbooksInjectorModule());
 
-        private EntityManagerFactory entityManagerFactory;
+	private EntityManagerFactory entityManagerFactory;
 
-        public EntityManagerFactory getEntityManagerFactory() {
-                return entityManagerFactory;
-        }
+	public EntityManagerFactory getEntityManagerFactory() {
+		return entityManagerFactory;
+	}
 
-        @Inject
-        public void setEntityManagerFactory(
-                        EntityManagerFactory entityManagerFactory) {
-                this.entityManagerFactory = entityManagerFactory;
-        }
+	@Inject
+	public void setEntityManagerFactory(
+			EntityManagerFactory entityManagerFactory) {
+		this.entityManagerFactory = entityManagerFactory;
+	}
 
-        @Before
-        public void prepare() {
-                injector.injectMembers(this);
-        }
+	@Before
+	public void prepare() {
+		injector.injectMembers(this);
+	}
 
-        private void bootstrap() {
-                EntityManager em = entityManagerFactory.createEntityManager();
-                em.getTransaction().begin();
-                Customer c = ObjectFactory.instance.createCustomer();
-                EventNewCustomer newCustomer = new EventNewCustomer();
-                newCustomer.setName("Fred");
-                c.processEvent(newCustomer);
-                em.persist(c);
-                em.getTransaction().commit();
-                em.close();
-        }
+	private void bootstrap() {
+		EntityManager em = entityManagerFactory.createEntityManager();
+		em.getTransaction().begin();
+		Customer c = ObjectFactory.instance.createCustomer();
+		EventNewCustomer newCustomer = new EventNewCustomer();
+		newCustomer.setName("Fred");
+		c.processEvent(newCustomer);
+		em.persist(c);
+		em.getTransaction().commit();
+		em.close();
+	}
 
-        @org.junit.Test
-        public void test() {
-                log.info("starting");
-                ObjectInjector.setInjector(injector);
-                bootstrap();
+	@org.junit.Test
+	public void test() {
+		log.info("starting");
+		ObjectInjector.setInjector(injector);
+		bootstrap();
 
-                EntityManager em = entityManagerFactory.createEntityManager();
-                em.getTransaction().begin();
+		EntityManager em = entityManagerFactory.createEntityManager();
+		em.getTransaction().begin();
 
-                Email email = ObjectFactory.instance.createEmail();
-                email.setEmail("fred.smith@googlecode.com");
+		Email email = ObjectFactory.instance.createEmail();
+		email.setEmail("fred.smith@googlecode.com");
 
-                EventAddEmail addEmail = new EventAddEmail();
-                addEmail.setEmail(email);
+		EventAddEmail addEmail = new EventAddEmail();
+		addEmail.setEmail(email);
 
-                Customer fred = Customer.Search.findByName(em, "Fred");
-                fred.processEvent(addEmail);
-                // no email should be added till the customer is activated
-                Assert.assertEquals(0, fred.getEmail().size());
+		Customer fred = Customer.Search.findByName(em, "Fred");
+		fred.processEvent(addEmail);
+		// no email should be added till the customer is activated
+		Assert.assertEquals(0, fred.getEmail().size());
 
-                fred.processEvent(new EventActivate());
-                fred.processEvent(addEmail);
+		fred.processEvent(new EventActivate());
+		fred.processEvent(addEmail);
 
-                em.persist(email);
-                em.getTransaction().commit();
-                em.close();
+		em.persist(email);
+		em.getTransaction().commit();
+		em.close();
 
-                em = entityManagerFactory.createEntityManager();
-                fred = Customer.Search.findByName(em, "Fred");
-                // now that the customer has been activated, the email can be added
-                Assert.assertEquals(1, fred.getEmail().size());
-                for (Email mail : fred.getEmail())
-                        mail.send("Hi", "This is a test message", "xUmlCompiler");
-                em.close();
-                log.info("finished");
-        }
+		em = entityManagerFactory.createEntityManager();
+		fred = Customer.Search.findByName(em, "Fred");
+		// now that the customer has been activated, the email can be added
+		Assert.assertEquals(1, fred.getEmail().size());
+		for (Email mail : fred.getEmail())
+			mail.send("Hi", "This is a test message", "xUmlCompiler");
+		em.close();
+		log.info("finished");
+	}
 
-        public static void main(String[] args) {
-                Test test = injector.getInstance(Test.class);
-                test.test();
-        }
+	public static void main(String[] args) {
+		Test test = injector.getInstance(Test.class);
+		test.test();
+	}
 
 }
