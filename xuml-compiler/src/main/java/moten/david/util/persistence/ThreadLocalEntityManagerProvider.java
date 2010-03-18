46254743239580a6d9210670db430feb7a8ab0f3
davidmoten <davidmoten@7688b277-2b4d-0410-bddd-c1844407bb12>
2010-03-18 11:23:24 +0000

    removed unused classes and updated with a class using Google Provider interface

--- xuml-compiler/src/main/java/moten/david/util/persistence/ThreadLocalEntityManagerProvider.java
+++ xuml-compiler/src/main/java/moten/david/util/persistence/ThreadLocalEntityManagerProvider.java
@@ -4,25 +4,20 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 
-public class ThreadLocalEntityManagerProvider implements EntityManagerProvider {
+public class ThreadLocalEntityManagerProvider implements Provider<EntityManager>{
 
-	private final EntityManagerFactory entityManagerFactory;
+	private final ThreadLocalEntityManagerFactory emf;
 
-	/**
-	 * expects a ThreadLocalEntityManagerFactory
-	 * 
-	 * @param entityManagerFactory
-	 */
 	@Inject
-	public ThreadLocalEntityManagerProvider(
-			EntityManagerFactory entityManagerFactory) {
-		this.entityManagerFactory = entityManagerFactory;
+	public ThreadLocalEntityManagerProvider(EntityManagerFactory emf) {
+		this.emf = new ThreadLocalEntityManagerFactory(emf);
 	}
-
+	
 	@Override
-	public EntityManager getEntityManager() {
-		return entityManagerFactory.createEntityManager();
+	public EntityManager get() {
+		return emf.createEntityManager();
 	}
-
+	
 }
