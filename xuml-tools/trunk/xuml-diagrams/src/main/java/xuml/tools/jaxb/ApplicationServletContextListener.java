129ce3e69e5fdb2b32258eac97cb55a2fb5d8ab1
davidmoten@gmail.com <davidmoten@gmail.com@7688b277-2b4d-0410-bddd-c1844407bb12>
2012-03-07 19:55:07 +0000

    persisted xml at startup

---
+++ xuml-tools/trunk/xuml-diagrams/src/main/java/xuml/tools/jaxb/ApplicationServletContextListener.java
@@ -0,0 +1,35 @@
+package xuml.tools.jaxb;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.io.IOUtils;
+
+public class ApplicationServletContextListener implements
+		ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		saveXmlTo("47");
+		saveXmlTo("48");
+	}
+
+	public void saveXmlTo(String id) {
+		try {
+			ModelPersistence.instance().save(
+					id,
+					IOUtils.toString(ApplicationServletContextListener.class
+							.getResourceAsStream("/xuml-sample-1.xml")));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+
+	}
+
+}
---
+++ xuml-tools/trunk/xuml-diagrams/src/main/java/xuml/tools/jaxb/ModelServlet.java
@@ -0,0 +1,47 @@
+package xuml.tools.jaxb;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+
+import xuml.tools.gae.Datastore;
+
+public class ModelServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		try {
+			String id = req.getParameter("id");
+			String result = ModelPersistence.instance().getXml(id);
+			resp.setContentType("text/xml");
+			if (result != null)
+				resp.getOutputStream().write(result.getBytes());
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String id = req.getParameter("id");
+		String xml = req.getParameter("xml");
+		// check can unmarshal
+		try {
+			new Marshaller().unmarshal(IOUtils.toInputStream(xml));
+			// save
+			ModelPersistence.instance().save(id, xml);
+		} catch (RuntimeException e) {
+			resp.setStatus(500);
+			resp.getWriter().println(e.getMessage());
+		}
+	}
+
+}
