8917f89d0ce6bd99c2c71532a1a9189522a74852
davidmoten@gmail.com <davidmoten@gmail.com@7688b277-2b4d-0410-bddd-c1844407bb12>
2012-03-12 11:15:47 +0000

    added code generator for java

--- xuml-tools/trunk/xuml-diagrams/src/main/java/xuml/tools/jaxb/CodeGeneratorJava.java
+++ xuml-tools/trunk/xuml-diagrams/src/main/java/xuml/tools/jaxb/CodeGeneratorJava.java
@@ -7,59 +7,65 @@ import java.util.Map;
 import xuml.metamodel.jaxb.Class;
 import xuml.metamodel.jaxb.Generalization;
 import xuml.metamodel.jaxb.Relationship;
+import xuml.metamodel.jaxb.System;
 
 import com.google.appengine.repackaged.com.google.common.collect.Maps;
 
 public class CodeGeneratorJava {
 
-	private static String key(Class cls) {
-		return cls.getDomain() + "." + cls.getName();
-	}
-
-	private static String key(Relationship r) {
-		return r.getNumber() + "";
-	}
-
-	private static String key(Generalization g) {
-		return g.getNumber() + "";
-	}
-
 	private static class Lookups {
-		Map<String, Class> classesByName = Maps.newHashMap();
-		Map<String, Relationship> relationshipsByName = Maps.newHashMap();
-		Map<String, Generalization> generalizationsByName = Maps.newHashMap();
+		final Map<String, Class> classesByName = Maps.newHashMap();
+		final Map<String, Relationship> relationshipsByName = Maps.newHashMap();
+		final Map<String, Generalization> generalizationsByName = Maps
+				.newHashMap();
+
+		public Lookups(System system) {
+			for (Class c : system.getClazz())
+				classesByName.put(key(c), c);
+			for (Relationship r : system.getRelationship())
+				relationshipsByName.put(key(r), r);
+			for (Generalization g : system.getGeneralization())
+				generalizationsByName.put(key(g), g);
+		}
+
+		public static String key(Class cls) {
+			return cls.getDomain() + "." + cls.getName();
+		}
+
+		public static String key(Relationship r) {
+			return r.getNumber() + "";
+		}
+
+		public static String key(Generalization g) {
+			return g.getNumber() + "";
+		}
+
 	}
 
 	public void generate(PrintStream out, xuml.metamodel.jaxb.System system,
-			Map<String, String> domainPackageNames, File destinationDirectory) {
+			Map<String, String> domainPackageNames, File destination) {
 		// create maps for classes, relationships, generalizations
-		Lookups lookups = new Lookups();
-		for (Class c : system.getClazz())
-			lookups.classesByName.put(key(c), c);
-		for (Relationship r : system.getRelationship())
-			lookups.relationshipsByName.put(key(r), r);
-		for (Generalization g : system.getGeneralization())
-			lookups.generalizationsByName.put(key(g), g);
+		Lookups lookups = new Lookups(system);
 
 		// create behaviour interfaces
-		createBehaviourInterfaces(lookups);
+		createBehaviourInterfaces(lookups, destination);
 		// create classes (impls)
-		createImplementations(lookups);
+		createImplementations(lookups, destination);
 		// create object factory
-		createObjectFactory(lookups);
+		createObjectFactory(lookups, destination);
 	}
 
-	private void createObjectFactory(Lookups lookups) {
+	private void createObjectFactory(Lookups lookups, File destination) {
 		// TODO Auto-generated method stub
 
 	}
 
-	private void createImplementations(Lookups lookups) {
+	private void createImplementations(Lookups lookups, File destination) {
 		// TODO Auto-generated method stub
 
 	}
 
-	private void createBehaviourInterfaces(Lookups lookups) {
+	private void createBehaviourInterfaces(Lookups lookups, File destination) {
 		// TODO Auto-generated method stub
 
 	}
