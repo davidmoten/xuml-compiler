577e1a0d8d66a5fa4787cc31be1656e3a975915e
davidmoten@gmail.com <davidmoten@gmail.com@7688b277-2b4d-0410-bddd-c1844407bb12>
2012-03-13 03:23:34 +0000

    generator dev

---
+++ xuml-tools/trunk/xuml-diagrams/src/test/java/xuml/tools/jaxb/compiler/CodeGeneratorJavaTest.java
@@ -0,0 +1,26 @@
+package xuml.tools.jaxb.compiler;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Map;
+
+import org.junit.Test;
+
+import xuml.tools.jaxb.Marshaller;
+import xuml.tools.jaxb.TstUtil;
+
+import com.google.common.collect.Maps;
+
+public class CodeGeneratorJavaTest {
+
+	@Test
+	public void test() throws FileNotFoundException {
+		xuml.metamodel.jaxb.System system = new Marshaller()
+				.unmarshal(new FileInputStream(TstUtil.SAMPLE_XML));
+		Map<String, String> domainPackageNames = Maps.newHashMap();
+		domainPackageNames.put("xuml", "miuml");
+		new CodeGeneratorJava(system, domainPackageNames).generate(new File(
+				"target/generated"));
+	}
+}
