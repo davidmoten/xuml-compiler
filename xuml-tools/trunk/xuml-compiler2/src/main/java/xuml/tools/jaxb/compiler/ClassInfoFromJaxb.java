94b89ab5a53126fb0c0ffc21159c1c635ccfa8f2
davidmoten@gmail.com <davidmoten@gmail.com@7688b277-2b4d-0410-bddd-c1844407bb12>
2012-03-17 05:49:56 +0000

    xuml-tools dev

--- xuml-tools/trunk/xuml-compiler2/src/main/java/xuml/tools/jaxb/compiler/ClassInfoFromJaxb.java
+++ xuml-tools/trunk/xuml-compiler2/src/main/java/xuml/tools/jaxb/compiler/ClassInfoFromJaxb.java
@@ -2,6 +2,7 @@ package xuml.tools.jaxb.compiler;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -10,13 +11,20 @@ import javax.xml.bind.JAXBElement;
 
 import xuml.metamodel.jaxb.Association;
 import xuml.metamodel.jaxb.AssociationEnd;
+import xuml.metamodel.jaxb.AssociativeReference;
 import xuml.metamodel.jaxb.Attribute;
 import xuml.metamodel.jaxb.Class;
 import xuml.metamodel.jaxb.DerivedAttribute;
+import xuml.metamodel.jaxb.Generalization;
 import xuml.metamodel.jaxb.Identifier;
 import xuml.metamodel.jaxb.IndependentAttribute;
+import xuml.metamodel.jaxb.IndependentAttributeType;
+import xuml.metamodel.jaxb.Multiplicity;
+import xuml.metamodel.jaxb.Reference;
 import xuml.metamodel.jaxb.ReferentialAttribute;
 import xuml.metamodel.jaxb.Relationship;
+import xuml.metamodel.jaxb.SuperclassReference;
+import xuml.metamodel.jaxb.ToOneReference;
 import xuml.tools.jaxb.Util;
 import xuml.tools.jaxb.compiler.ClassInfoSample.MyEvent;
 import xuml.tools.jaxb.compiler.ClassInfoSample.MyIndependentAttribute;
@@ -31,6 +39,7 @@ import com.google.common.collect.Sets;
 
 public class ClassInfoFromJaxb implements ClassInfo {
 
+	private static final String UNEXPECTED = "unexpected";
 	private final Class cls;
 	private final String packageName;
 	private final Lookups lookups;
@@ -203,8 +212,19 @@ public class ClassInfoFromJaxb implements ClassInfo {
 
 	@Override
 	public MyIndependentAttribute getPrimaryId() {
-		// TODO Auto-generated method stub
-		return null;
+		Collection<Attribute> attributes = getIdentifiers().get(BigInteger.ONE);
+		if (attributes.size() == 1) {
+			Attribute attribute = attributes.iterator().next();
+			String fieldName = Util.toJavaIdentifier(attribute.getName());
+			return new MyIndependentAttribute(fieldName,
+					Util.toColumnName(fieldName), getType(cls, attribute),
+					false, null);
+		} else
+			// don't use embedded id but rather create an arbitrary id
+			return new MyIndependentAttribute("id", Util.toTableIdName(cls
+					.getName()), new Type(
+					toJavaType(IndependentAttributeType.ARBITRARY_ID), null,
+					false), false, "");
 	}
 
 	@Override
@@ -271,6 +291,179 @@ public class ClassInfoFromJaxb implements ClassInfo {
 	public Set<String> getImports() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	private Type getType(Class cls, Attribute a) {
+		MyAttributeType t = getMyAttributeType(cls, a);
+		if (t.multiple)
+			return new Type(Set.class.getName(), Lists.newArrayList(new Type(
+					toJavaType(t.type), null, false)), false);
+		return new Type(toJavaType(t.type), null, false);
+	}
+
+	private static class MyAttributeType {
+		IndependentAttributeType type;
+		boolean multiple;
+
+		public MyAttributeType(IndependentAttributeType type, boolean multiple) {
+			super();
+			this.type = type;
+			this.multiple = multiple;
+		}
+	}
+
+	private void log(String message, Object... objects) {
+		java.lang.System.out.format(message, objects);
+	}
+
+	/**
+	 * Recursively travels relationship paths and returns the type of a referred
+	 * attribute.
+	 * 
+	 * @param cls
+	 * @param a
+	 * @return
+	 */
+	private MyAttributeType getMyAttributeType(Class cls, Attribute a) {
+		log("getting type of %s.%s", cls.getName(), a.getName());
+
+		if (a instanceof IndependentAttribute) {
+			return new MyAttributeType(((IndependentAttribute) a).getType(),
+					false);
+		} else if (a instanceof DerivedAttribute) {
+			return new MyAttributeType(((DerivedAttribute) a).getType(), false);
+		} else if (a instanceof ReferentialAttribute) {
+			ReferentialAttribute r = (ReferentialAttribute) a;
+			Reference ref = r.getReferenceBase().getValue();
+			if (ref instanceof ToOneReference) {
+				ToOneReference t = (ToOneReference) ref;
+				MyAttributeType result = getToOneReferenceType(cls, a, r, t);
+				return result;
+			} else if (ref instanceof SuperclassReference) {
+				SuperclassReference t = (SuperclassReference) ref;
+				MyAttributeType otherType = getGeneralizationType(cls, a, r, t);
+				return otherType;
+			} else if (ref instanceof AssociativeReference) {
+				AssociativeReference t = (AssociativeReference) ref;
+				MyAttributeType otherType = getAssociativeType(cls, a, r, t);
+				return otherType;
+			} else {
+				throw new RuntimeException(UNEXPECTED);
+			}
+		} else
+			throw new RuntimeException("unexpected attribute type " + a);
+	}
+
+	private MyAttributeType getAssociativeType(Class cls, Attribute a,
+			ReferentialAttribute r, AssociativeReference t) {
+		Association ass = lookups.getAssociation(cls.getDomain(),
+				t.getRelationship());
+		Class other;
+		if (t.getSide().intValue() == 1)
+			other = lookups
+					.getClass(ass.getDomain(), ass.getClass1().getName());
+		else
+			other = lookups
+					.getClass(ass.getDomain(), ass.getClass2().getName());
+		String otherName;
+		if (r.getOtherName() == null)
+			otherName = r.getName();
+		else
+			otherName = r.getOtherName();
+		log("looking up attribute %s %s %s", other.getDomain(),
+				other.getName(), otherName);
+		Attribute otherAttribute = lookups.getAttribute(other.getDomain(),
+				other.getName(), otherName);
+		MyAttributeType otherType = getMyAttributeType(other, otherAttribute);
+		return otherType;
+	}
+
+	private MyAttributeType getGeneralizationType(Class cls, Attribute a,
+			ReferentialAttribute r, SuperclassReference t) {
+		Generalization g = lookups.getGeneralization(cls.getDomain(),
+				t.getRelationship());
+		if (g == null)
+			throw new RuntimeException("did not find association");
+		Class other = getOtherClass(g);
+		String otherName;
+		if (r.getOtherName() == null)
+			otherName = r.getName();
+		else
+			otherName = r.getOtherName();
+		log("looking up attribute %s %s %s", other.getDomain(),
+				other.getName(), otherName);
+		Attribute otherAttribute = lookups.getAttribute(other.getDomain(),
+				other.getName(), otherName);
+		MyAttributeType otherType = getMyAttributeType(other, otherAttribute);
+		return otherType;
+	}
+
+	private Class getOtherClass(Generalization g) {
+		return lookups.getClass(g.getDomain(), g.getSuperclass());
+	}
+
+	private MyAttributeType getToOneReferenceType(Class cls, Attribute a,
+			ReferentialAttribute r, ToOneReference t) {
+		Association ass = lookups.getAssociation(cls.getDomain(),
+				t.getRelationship());
+		if (ass == null)
+			throw new RuntimeException("did not find association");
+		Class other = getOtherClass(cls, ass);
+		AssociationEnd otherEnd = getOtherEnd(cls, ass);
+		String otherName = r.getOtherName();
+		if (otherName == null)
+			otherName = a.getName();
+		log("looking up attribute %s %s %s", other.getDomain(),
+				other.getName(), otherName);
+		Attribute otherAttribute = lookups.getAttribute(other.getDomain(),
+				other.getName(), otherName);
+		MyAttributeType otherType = getMyAttributeType(other, otherAttribute);
+		MyAttributeType result;
+		if (otherEnd.getMultiplicity().equals(Multiplicity.MANY)
+				|| otherEnd.getMultiplicity().equals(Multiplicity.ONE_MANY)) {
+			result = new MyAttributeType(otherType.type, true);
+		} else
+			result = otherType;
+		return result;
+	}
+
+	private Class getOtherClass(Class cls, Association a) {
+		if (cls.getDomain().equals(a.getDomain())) {
+			if (cls.getName().equals(a.getClass1().getName()))
+				return lookups.getClass(a.getDomain(), a.getClass2().getName());
+			else
+				return lookups.getClass(a.getDomain(), a.getClass1().getName());
+		} else
+			throw new RuntimeException(UNEXPECTED);
+	}
+
+	private AssociationEnd getThisEnd(Class cls, Association a) {
+		if (cls.getDomain().equals(a.getDomain())) {
+			if (cls.getName().equals(a.getClass1().getName()))
+				return a.getClass1();
+			else
+				return a.getClass2();
+		} else
+			throw new RuntimeException(UNEXPECTED);
+	}
+
+	private static String toJavaType(IndependentAttributeType type) {
+		if (type.equals(IndependentAttributeType.ARBITRARY_ID)) {
+			return "Long";
+		} else if (type.equals(IndependentAttributeType.BOOLEAN)) {
+			return "Boolean";
+		} else if (type.equals(IndependentAttributeType.DATE)) {
+			return "String";
+		} else if (type.equals(IndependentAttributeType.DECIMAL)) {
+			return "BigDecimal";
+		} else if (type.equals(IndependentAttributeType.INTEGER)) {
+			return "BigInteger";
+		} else if (type.equals(IndependentAttributeType.STRING)) {
+			return "String";
+		} else if (type.equals(IndependentAttributeType.TIMESTAMP)) {
+			return "java.util.Date";
+		} else
+			throw new RuntimeException("no java type implemented for " + type);
 	}
 
 }
