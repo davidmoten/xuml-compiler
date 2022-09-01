7e44525f1bfc7234ac1f007294a2c945dc6bff34
Dave Moten <davidmoten@gmail.com>
2022-09-01 11:04:28 +1000

    Upgrade hibernate from 3.2.x to 5.6.11, upgrade other deps and plugins  (#6)
    * upgrade hibernate from 3.2.x to 5.6.x
    
    * remove .travis.yml
    
    * remove ejb3-persistence dep
    
    * bump hsqldb from 1.8.0.7 to 1.8.0.10
    
    * fix persistence.xml so that schemas auto-create
    
    * upgrade freemarker, ant, equinox app, eclipse core runtime
    
    * bump swingx from 0.9.2 to 1.6.1
    
    * add README.md, fix poms
    
    * Update README.md
    
    * Update README.md

--- xuml-compiler/src/main/java/moten/david/util/hibernate/utc/TimeType.java
+++ xuml-compiler/src/main/java/moten/david/util/hibernate/utc/TimeType.java
@@ -7,51 +7,39 @@ import java.sql.SQLException;
 import java.sql.Types;
 
 import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
 
 /**
  * Like a Hibernate time, but using the UTC TimeZone (not the default TimeZone).
  */
 public class TimeType extends HibernateUTC {
 
-	private static Logger log = Logger.getLogger(TimeType.class);
-	protected static int[] SQL_TYPES_TIME = { Types.TIME };
+    private static Logger log = Logger.getLogger(TimeType.class);
+    protected static int[] SQL_TYPES_TIME = { Types.TIME };
 
-	/**
-	 * @see net.sf.hibernate.UserType#sqlTypes()
-	 */
-	@Override
-	public int[] sqlTypes() {
-		return SQL_TYPES_TIME;
-	}
+    @Override
+    public int[] sqlTypes() {
+        return SQL_TYPES_TIME;
+    }
 
-	/**
-	 * @see net.sf.hibernate.UserType#deepCopy(java.lang.Object)
-	 */
-	public Object deepCopy(Object value) {
-		return (value == null) ? null : new java.sql.Time(((Date) value)
-				.getTime());
-	}
+    public Object deepCopy(Object value) {
+        return (value == null) ? null : new java.sql.Time(((Date) value).getTime());
+    }
 
-	/**
-	 * @see net.sf.hibernate.UserType#nullSafeGet(java.sql.ResultSet,
-	 *      java.lang.String[], java.lang.Object)
-	 */
-	public Object nullSafeGet(ResultSet rs, String[] names, Object owner)
-			throws SQLException {
-		log.debug("returning '" + rs.getTime(names[0], sUTCCalendar)
-				+ "' from column:" + names[0]);
-		return rs.getTime(names[0], sUTCCalendar);
-	}
+    @Override
+    public Object nullSafeGet(ResultSet rs, String[] names, SharedSessionContractImplementor session, Object owner)
+            throws HibernateException, SQLException {
+        log.debug("returning '" + rs.getTime(names[0], sUTCCalendar) + "' from column:" + names[0]);
+        return rs.getTime(names[0], sUTCCalendar);
+    }
 
-	/**
-	 * @see net.sf.hibernate.UserType#nullSafeSet(java.sql.PreparedStatement,
-	 *      java.lang.Object, int)
-	 */
-	public void nullSafeSet(PreparedStatement st, Object value, int index)
-			throws SQLException {
-		if (!(value instanceof java.sql.Time))
-			value = deepCopy(value);
-		log.debug("binding '" + value + "' to parameter:" + index);
-		st.setTime(index, (java.sql.Time) value, sUTCCalendar);
-	}
+    @Override
+    public void nullSafeSet(PreparedStatement st, Object value, int index, SharedSessionContractImplementor session)
+            throws HibernateException, SQLException {
+        if (!(value instanceof java.sql.Time))
+            value = deepCopy(value);
+        log.debug("binding '" + value + "' to parameter:" + index);
+        st.setTime(index, (java.sql.Time) value, sUTCCalendar);
+    }
 }
